500e3e934ac7d376c2bc92d9f92f8f6448868189
firdouskhosa <firdouskhosa@gmail.com>
2023-09-16 12:31:42 +0500

    updated port

--- index.js
+++ index.js
@@ -55,21 +55,21 @@ app.use(
   })
 );
 // Sequelize setup
-// const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-//   host: "localhost",
-//   dialect: "mysql",
-// });
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: "localhost",
+  dialect: "mysql",
+});
 
 
-const sequelize = new Sequelize(
-  "friendshipdares",
-  "new_username",
-  "MyP@ssw0rd2023",
-  {
-    host: "localhost",
-    dialect: "mysql",
-  }
-);
+// const sequelize = new Sequelize(
+//   "friendshipdares",
+//   "new_username",
+//   "MyP@ssw0rd2023",
+//   {
+//     host: "localhost",
+//     dialect: "mysql",
+//   }
+// );
 
 sequelize.sync().then(() => {
   console.log("Database synchronized.");
@@ -353,6 +353,7 @@ app.get("/getQuestions", async (req, res) => {
 });
 
 app.get("/getQuestionWithAnswerById/:q_id", async (req, res) => {
+  console.log(req.headers);
   await verifyToken(req, res);
   try {
     const questionWithAnswerById = await Question.findOne({
