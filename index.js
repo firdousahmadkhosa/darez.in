aea782f4f6eaa50bd3147a4923ce34ee6bb8b4b8
firdouskhosa <firdouskhosa@gmail.com>
2023-09-16 12:33:18 +0500

    updated port

--- index.js
+++ index.js
@@ -55,21 +55,21 @@ app.use(
   })
 );
 // Sequelize setup
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: "localhost",
-  dialect: "mysql",
-});
+// const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+//   host: "localhost",
+//   dialect: "mysql",
+// });
 
 
-// const sequelize = new Sequelize(
-//   "friendshipdares",
-//   "new_username",
-//   "MyP@ssw0rd2023",
-//   {
-//     host: "localhost",
-//     dialect: "mysql",
-//   }
-// );
+const sequelize = new Sequelize(
+  "friendshipdares",
+  "new_username",
+  "MyP@ssw0rd2023",
+  {
+    host: "localhost",
+    dialect: "mysql",
+  }
+);
 
 sequelize.sync().then(() => {
   console.log("Database synchronized.");
